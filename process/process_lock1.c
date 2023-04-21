fe7b16eb4765c7723e8ab529d95f2ba262d7a858
lai0yu <lai0yu@outlook.com>
2023-04-21 15:12:09 +0800

    process lock

---
+++ process/process_lock1.c
@@ -0,0 +1,31 @@
+#include <sys/file.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+	int fd = open("test.c",O_RDWR|O_APPEND);
+	if(fd < 0){
+		perror("open error\n");
+		return -1;
+	}
+
+	char buf[30] = "唱跳rap篮球\n";
+	while(1){
+		sleep(1);
+		flock(fd, LOCK_EX);
+		printf("唱锁\n");
+		sleep(3);
+		write(fd, buf, strlen(buf));
+		printf("%s\n", buf);
+
+		sleep(50);
+		printf("解唱锁\n");
+		flock(fd,LOCK_UN);
+		printf("解锁成功\n");
+	}
+	return 0;
+}
