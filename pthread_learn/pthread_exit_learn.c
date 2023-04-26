e22f785383216cd9d876cc5fad335f15155477a0
lai0yu <lai0yu@outlook.com>
2023-04-26 11:44:36 +0800

    fix pthread exit learn

--- pthread_learn/pthread_exit_learn.c
+++ pthread_learn/pthread_exit_learn.c
@@ -5,15 +5,13 @@ void* thread_work(void* arg){
 	printf("I am working, args is :%s \n",(char*)arg);
 	char* dead_word = "I hava to exit my work";
 	pthread_exit(dead_word);
-
 	printf("finish work\n");
 }
 
 int main(void){
-	
 	pthread_t thread1;
-	char* arg = "testarg";
+	char * arg = "give thread1 arg";
 	pthread_create(&thread1, NULL, thread_work, arg);
-    sleep(1);
+	sleep(1);
 	return 0;
 }
