5a046b3a8e3b9e93ab69672baa3155823c19ecae
lai0yu <lai0yu@outlook.com>
2023-05-03 17:58:58 +0800

    add exec learn

---
+++ process_learn/exec/execv_learn.c
@@ -0,0 +1,17 @@
+#include <cstddef>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void){
+
+	printf("Current PID:%d\n",getpid());
+
+	char* argv[] = {"ls","-l",NULL};
+
+	int ret = execv("/bin/ls",argv);
+
+	printf("This line will not be print after exec\n");
+
+	return 0;
+}
---
+++ process_learn/exec/execvpe_learn.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+int main(void){
+	printf("Current PID:%d\n",getpid());
+
+	char* argv[] = {"./new",NULL};
+
+	char* envp[] = {"AA","BB","CC","DD","NULL"};
+
+	int ret = execvpe("new",argv,envp);
+	
+	printf("This line will not been print after exec\n");
+
+	return 0;
+}
