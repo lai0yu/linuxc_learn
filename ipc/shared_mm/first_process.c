0a268fd53b7d05e2058f465e6fb58fc5ee2367ab
lai0yu <lai0yu@outlook.com>
2023-05-04 19:12:29 +0800

    refactor dir structor

--- ipc/shared_mm/first_process.c
+++ ipc/shared_mm/first_process.c
@@ -2,16 +2,17 @@
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <stdio.h>
-int main(void){
-	
-	key_t key = ftok("1.c",1);
-	if(key_t < 0){
+int main(void)
+{
+
+	key_t key = ftok("1.c", 1);
+	if (key < 0)
+	{
 		perror("ftok key error\n");
 		return -1;
 	}
-	printf("key:%#x\n",key);
+	printf("key:%#x\n", key);
 
-	int shmid = shmget(key,200,IPC_CREAT|0666);
+	int shmid = shmget(key, 200, IPC_CREAT | 0666);
 	return 0;
-
 }
--- socket/test.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-#include <pthread.h>
-
-int main(int argc, char* argv){
-	pthread_create(pthread_t *restrict __newthread, const pthread_attr_t *restrict __attr, dd`:, void *restrict __arg)
-}
-
