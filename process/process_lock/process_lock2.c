bc122b1346b9d62d4b835a8c715c0932855f5afe
lai0yu <lai0yu@outlook.com>
2023-05-05 00:19:44 +0800

    rename process_lock file name

--- process/process_lock/process_lock2.c
+++ process/process_lock/process_lock2.c
@@ -7,27 +7,29 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
-
-int main(){
-	int fd = open("test.c",O_RDWR|O_APPEND);
-	if(fd < 0){
+int main()
+{
+	int fd = open("test.c", O_RDWR | O_APPEND);
+	if (fd < 0)
+	{
 		perror("open error");
 		return -1;
 	}
 
 	char buf[30] = "鸡你太美\n";
 
-	while(1){
+	while (1)
+	{
 		sleep(1);
-		flock(fd,LOCK_EX);
+		flock(fd, LOCK_EX);
 		printf("鸡锁\n");
 
 		sleep(3);
-		write(fd,buf,strlen(buf));
-		printf("%s\n",buf);
+		write(fd, buf, strlen(buf));
+		printf("%s\n", buf);
 
 		printf("解鸡锁\n");
-		flock(fd,LOCK_UN);
+		flock(fd, LOCK_UN);
 		printf("解锁成功\n");
 	}
 
