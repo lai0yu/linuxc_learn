407c67807efed98ef6348655ba0a7c8bf31a31d5
lai0yu <lai0yu@outlook.com>
2023-05-06 14:47:01 +0800

    prepare for sub module

---
+++ cqq/cqqserver/cqqserver.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+
+int main(int argc, char *argv) {
+
+  return 0;
+}
